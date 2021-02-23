56465747ab0890f790238661d4820206a2a8cf0c
Mariat Ndagire <mariatndagire28@gmail.com>
2021-02-23 11:28:46 +0300

    initial travis Commit

---
+++ app.js
@@ -0,0 +1,5 @@
+function add(a,b){
+    return a+b;
+}
+let addition= add(2,3);
+module.exports=addition;
---
+++ apptest.js
@@ -0,0 +1,9 @@
+const app=require("./app");
+let assert=require("chai").assert;
+
+describe("integrating TravisCI with github", test);
+function test(){
+        it("should return a number", ()=>{
+            assert.isNumber(app,"Result should be a number")
+        });
+}
